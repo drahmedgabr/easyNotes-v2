ea645e5a2dec1745c6bc926504794819b94d0ef8
drahmedgabr <ahmadal4ever@gmail.com>
2024-11-19 18:07:21 +0200

    shownotes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const noteInput = document.getElementById("noteInput");
+const notesDiv = document.getElementById("notesDiv");
+const emptyDiv = document.getElementById("emptyDiv");
 var notesArray = [];
 
 getNotes();
@@ -11,7 +13,7 @@ function addNote() {
         notesArray.push(newNote);
         noteInput.value = "";
         saveNotes();
-        console.log(notesArray);
+        getNotes();
     }
 }
 
@@ -26,6 +28,44 @@ function getNotes() {
         console.log("no notes found in localstorage");
     } else {
         notesArray = JSON.parse(savedNotes);
+        showNotes();
     }
+}
+
+function showNotes() {
+
+    notesDiv.innerHTML = "";
+
+    if (notesArray.length > 0) {
+
+        emptyDiv.style.display = "none";
+        notesDiv.style.display = "block";
+
+        for (let index = 0; index < notesArray.length; index++) {
+            const element = notesArray[index];
+            
+            const newDiv = document.createElement("div");
+    
+            const newP = document.createElement("p");
+            newP.innerText = element;
+            newDiv.appendChild(newP);
+    
+            const deleteIcon = document.createElement("i");
+            deleteIcon.className = "bi bi-trash3";
+            newDiv.appendChild(deleteIcon);
+    
+            notesDiv.appendChild(newDiv);
+    
+        }
+    
+        const lastDiv = document.createElement("div");
+        lastDiv.id = "lastDiv";
+    
+        notesDiv.appendChild(lastDiv);
+    } else {
+        emptyDiv.style.display = "flex";
+        notesDiv.style.display = "none";
+    }
+
 }
 
